0f7c345197e8a459f30b3e7cbe3233e856410186
renztp <tullaorenz@gmail.com>
2026-02-04 16:30:19 +0800

    added changes to pages config

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,14 @@
 import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
 import { defineCollection } from 'astro:content';
 
 const photoSets = defineCollection({
-  loader: glob({ pattern: '*.md', base: './src/content/sets' })
+  loader: glob({ pattern: '*.md', base: './src/content/sets' }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    photos: z.array(z.string())
+  })
 });
 
 export const collections = { photoSets };
